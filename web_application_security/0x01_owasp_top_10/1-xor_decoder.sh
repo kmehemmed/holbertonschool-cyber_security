704f5ac283660eb3b928ee86dc92f9f40c8476ec
kmehemmed <kelbiyevmehemmed90@gmail.com>
2026-07-04 07:18:21 +0000

    salamsemaye

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 input_hash="${1#\{xor\}}"
-decoded_base64=$(echo -n "$input_hash" | base64 -d 2>/dev/null)
+decoded_hex=$(echo -n "$input_hash" | base64 -d 2>/dev/null | od -An -v -t x1)
 
-echo -n "$decoded_base64" | od -An -v -t x1 | tr -d '\n' | awk '{
-    for(i=1; i<=NF; i++) {
-        printf "%c", xor(strtonum("0x"$i), 0x5f)
-    }
-    print ""
-}'
+for hex in $decoded_hex; do
+    dec=$((16#$hex))
+    xor_dec=$((dec ^ 95))
+    printf "\\$(printf '%03o' $xor_dec)"
+done
+echo ""
